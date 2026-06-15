8afaf0d742986029a24efdc2b0a7c2aa77b4064e
Aakash Sriram <194975502+Aakash-Sriram@users.noreply.github.com>
2026-06-16 00:24:48 +0530

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+### Download Youtube videos in mp3 format
+
+# Preparation
+
+1.Create songs.txt folder
+
+2.Add the links of videos you want to download inside songs.txt , each link comma seperated [Follow format in @sample.txt]
+
+3.Run the python script
+
+```
+python3 main.py
+
+```
---
+++ main.py
@@ -0,0 +1,18 @@
+import subprocess
+WD="/home/smthing/songs" # Change this to your working directory
+DOWNLOAD_DIR=f"{WD}/downloads" # Change this to your desired download directory
+subprocess.run(["mkdir", "-p", f"{DOWNLOAD_DIR}"])
+subprocess.run(["rm", "-rf", f"{WD}"])
+songs=[]
+with open(f"{WD}/songs.txt","r") as f:
+    song = f.read()
+    songs = song.split("\n")
+for song in songs:
+    song=song[:len(song)-1]
+    try:
+        print(f"Downloading {song}...")
+        subprocess.run([
+    "yt-dlp","-x","--audio-format", "mp3","--audio-quality", "0","-o", f"{DOWNLOAD_DIR}/%(title)s.%(ext)s",f"{song}"])
+    except Exception as e:
+        print(f"Error downloading {song}: {e}")
+print(songs)
